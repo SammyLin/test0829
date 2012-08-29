b87058cc11be873865b824d5074f8f4673b58908
Sammy <sammylintw@gmail.com>
2012-08-29 17:37:46 +0800

    fix

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/mis/apps/test0829/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=mis
 set -u
 
 OLD_PIN="$PID.oldbin"
